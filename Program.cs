2a1c6824a7b953ab54f1a410fc127d9ab72e62cd
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-07-14 12:43:22 +0300

    Upload first version

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("How many guids do you need: ");
+
+int count;
+bool success = int.TryParse(Console.ReadLine(), out count);
+
+while (!success)
+{
+    Console.WriteLine("Invalid input!");
+    Console.Write("How many guids do you need: ");
+    success = int.TryParse(Console.ReadLine(), out count);
+}
+
+Console.WriteLine("Generating...");
+
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine(Guid.NewGuid());
+}
+
+Console.WriteLine("Done");
